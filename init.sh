c421575806fd5678f44524dbd65b0de4ace059f5
Kyle Brennan <kylos101@gmail.com>
2022-03-21 18:33:24 +0000

    stress it

--- init.sh
+++ init.sh
@@ -1,5 +1,7 @@
 #!/bin/bash -e
 
+sudo apt install stress -y
+
 echo "Fire it up"
 
 DOCKER_CONFIG=${DOCKER_CONFIG:-$HOME/.docker}
@@ -7,8 +9,6 @@ mkdir -p $DOCKER_CONFIG/cli-plugins
 curl -sL https://github.com/docker/compose/releases/download/v2.2.3/docker-compose-linux-x86_64 -o $DOCKER_CONFIG/cli-plugins/docker-compose
 chmod +x $DOCKER_CONFIG/cli-plugins/docker-compose
 
-echo "done"
+docker compose up
 
 # while sleep 1;do tput sc;tput cup 0 $(($(tput cols)-38));echo `date` ${SECONDS};tput rc;done
-
-echo "I got here again, again more, and again"
