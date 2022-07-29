8501f7158a9b4020e9f660422b755e67fc9d1b5c
Kyle Brennan <kylos101@gmail.com>
2022-07-29 21:14:00 +0000

    And go

--- load-test.sh
+++ load-test.sh
@@ -1,11 +1,9 @@
 #!/bin/bash -e
-for i in {73..90}
+for i in {75..95}
 do
     git checkout -b kyleb/"$i"-prebuild
     echo "echo another test $i" >> init.sh
     git add .
     git commit -m "$i another test"
     git push --set-upstream origin kyleb/"$i"-prebuild
-    git checkout main
-    git pull --ff-only
 done
