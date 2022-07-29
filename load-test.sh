94beedf0163a223d736d6a66ee0b9bc7ec4f1d81
Kyle Brennan <kylos101@gmail.com>
2022-07-29 21:16:32 +0000

    One moar!

--- load-test.sh
+++ load-test.sh
@@ -1,9 +1,9 @@
 #!/bin/bash -e
-for i in {75..95}
+for i in {76..96}
 do
     git checkout -b kyleb/"$i"-prebuild
     echo "echo another test $i" >> init.sh
     git add .
     git commit -m "$i another test"
-    git push --set-upstream origin kyleb/"$i"-prebuild
+    git push origin kyleb/"$i"-prebuild
 done
