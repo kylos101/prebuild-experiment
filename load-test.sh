51728ed709bec469bc6d125d539d5a26683b5d87
Kyle Brennan <kyle@gitpod.io>
2023-05-01 20:05:19 +0000

    Mon May  1 08:05:19 PM UTC 2023

--- load-test.sh
+++ load-test.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -e
 # go again again
-for i in {104..125}
+for i in {601..606}
 do
     git checkout -b kyleb/"$i"-prebuild
     echo "echo another test $i" >> init.sh
---
+++ test-abort-feature.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+while true = true; do
+    FOO="$(date)"
+    touch bar.txt && cat $FOO > bar.txt
+    git add . && git commit -m "$FOO"
+    git push
+    echo "Sleeping 5 seconds at $(date)..."
+    sleep 5
+    echo "Resuming  at $(date)..."
+done
