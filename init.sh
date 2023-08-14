a91d6eb58cc3f2f2db55590addf9c3a736c1d002
kylos101 <kylos101@gmail.com>
2023-08-14 00:34:17 +0000

    962 another test

--- init.sh
+++ init.sh
@@ -277,3 +277,4 @@ echo another test 958
 echo another test 959
 echo another test 960
 echo another test 961
+echo another test 962
--- load-test.sh
+++ load-test.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -e
 # go again again
 
-for i in {960..961}
+for i in {962..964}
 do
     git checkout -b kyleb/"$i"-prebuild
     echo "echo another test $i" >> init.sh
