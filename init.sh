57b414c901f3d6c025331a43d9e5648a01d562de
Kyle Brennan <kylos101@gmail.com>
2024-01-19 21:57:43 +0000

    clarification on behavior

--- init.sh
+++ init.sh
@@ -2,6 +2,16 @@
 
 echo "Fire it up"
 
+if [[ -n "$GITPOD_HEADLESS:-" ]]; then
+    echo "$(date) is-headless-with-default" >> /workspace/init-test
+fi
+sleep 5
+if [[ -n "$GITPOD_HEADLESS" ]]; then
+    echo "$(date) is-headless-no-default" >> /workspace/init-test
+fi
+sleep 5
+echo "$(date) done" >> /workspace/init-test
+
 DOCKER_CONFIG=${DOCKER_CONFIG:-$HOME/.docker}
 mkdir -p $DOCKER_CONFIG/cli-plugins
 curl -sL https://github.com/docker/compose/releases/download/v2.2.3/docker-compose-linux-x86_64 -o $DOCKER_CONFIG/cli-plugins/docker-compose
