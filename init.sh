ffb82b329c7af21420563f8e748dc198f9d31ccd
Kyle Brennan <kylos101@gmail.com>
2024-02-06 18:56:13 +0000

    wont prebuild

--- init.sh
+++ init.sh
@@ -17,7 +17,7 @@ mkdir -p $DOCKER_CONFIG/cli-plugins
 curl -sL https://github.com/docker/compose/releases/download/v2.2.3/docker-compose-linux-x86_64 -o $DOCKER_CONFIG/cli-plugins/docker-compose
 chmod +x $DOCKER_CONFIG/cli-plugins/docker-compose
 
-echo "done"
+echo "done again"
 
 sleep 15
 
